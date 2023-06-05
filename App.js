8f8c7315263a27730d304392571f6b87646543fb
Vinita Kumari <vinita.kumari@qubited.com>
2023-06-05 15:29:36 +0530

    First assignment

---
+++ App.js
@@ -0,0 +1,7 @@
+
+const heading = React.createElement('h1', null, "Hello World From React");
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(root);
+root.render(heading)
