9e323c68a14ddfed05344153b0ca3810ca5f41fd
Vinita Kumari <vinita.kumari@qubited.com>
2023-06-06 00:00:16 +0530

    second assignment

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
+import React from 'react';
+import ReactDOM from "react-dom/client";
 
 const heading = React.createElement('h1', null, "Hello World From React");
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 console.log(root);
-root.render(heading)
+root.render(heading);
